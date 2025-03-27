c40942e41b1083f8bda825c5121c75a0fc4e7c4c
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-03-27 20:10:22 +0100

    Bump version

--- src/main/java/mod/acgaming/jockeys/Jockeys.java
+++ src/main/java/mod/acgaming/jockeys/Jockeys.java
@@ -19,7 +19,7 @@ import mod.acgaming.jockeys.util.JockeysHelper;
 public class Jockeys
 {
     public static final String MOD_ID = "jockeys";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @Instance
     public static Jockeys instance;
